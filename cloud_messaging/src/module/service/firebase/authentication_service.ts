28b5d7ce13f8e759cc8d652db2b84d57f34baa98
kakeru-ikeda <kakerumail1451@gmail.com>
2024-11-14 01:02:30 +0900

    refactor: adminモジュールのインポート方法を修正し、IDトークンの検証処理を簡素化

--- cloud_messaging/src/module/service/firebase/authentication_service.ts
+++ cloud_messaging/src/module/service/firebase/authentication_service.ts
@@ -1,12 +1,11 @@
 import { FirebaseCore } from "../core/firebase_core";
-import admin from 'firebase-admin';
+import * as admin from 'firebase-admin';
 
 export class AuthenticationService extends FirebaseCore {
     async verifyIdToken(idToken: string) {
         console.log('Verifying ID token...');
         try {
-            const auth = admin.auth(this.app);
-            const decodedToken = await auth.verifyIdToken(idToken);
+            const decodedToken = await admin.auth().verifyIdToken(idToken);
             console.log('Successfully verified ID token:', decodedToken);
             return decodedToken;
         } catch (error) {
