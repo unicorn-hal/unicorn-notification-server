09a3c41a955accb25fedface8905aa57a9c8ee00
kakeru-ikeda <kakerumail1451@gmail.com>
2024-12-09 20:53:12 +0900

    feat: CORSミドルウェアを更新し、OPTIONSメソッドの処理を雨にする

--- cloud_messaging/src/index.ts
+++ cloud_messaging/src/index.ts
@@ -9,10 +9,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Middleware
-app.use((_, res, next) => {
+app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    res.header("Access-Control-Allow-Methods", "GET, POST");
+    res.header("Access-Control-Allow-Methods", "POST, OPTIONS");
+    if (req.method === 'OPTIONS') {
+        res.sendStatus(204).end();
+        return;
+    }
     next();
 });
 
